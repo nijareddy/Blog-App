c075684fa03c345f804bc3b0629a7b33037410a9
nijareddy <140965809+nijareddy@users.noreply.github.com>
2024-08-25 15:08:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,8 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-  origin: 'https://blog-app-react-pi-blush.vercel.app',
-  methods:["GET","POST","PUT","DELETE"],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: 'https://localhost:3000',
+  
 }));
 
 const dbpath = path.join(__dirname, 'database.db');
